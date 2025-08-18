c1688a9a459f6fa969cfd18bfc6e3bab0737af51
jab1897 <138234117+jab1897@users.noreply.github.com>
2025-08-18 10:18:02 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
-import './styles.css'
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Shell from "./shell/Shell.jsx";
+import ErrorBoundary from "./ErrorBoundary.jsx";
+import Home from "./pages/Home.jsx";
+import District from "./pages/District.jsx";
+import Campus from "./pages/Campus.jsx";
 
 export default function App() {
-  return (<>
-    <Header />
-    <Outlet />
-  </>)
+  return (
+    <BrowserRouter>
+      <ErrorBoundary>
+        <Shell>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/district/:districtId" element={<District />} />
+            <Route path="/campus/:campusId" element={<Campus />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Shell>
+      </ErrorBoundary>
+    </BrowserRouter>
+  );
 }
