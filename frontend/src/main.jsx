fca7102b7d35bc2eed0bc993488888d22f0dd45d
jab1897 <138234117+jab1897@users.noreply.github.com>
2025-08-18 10:29:42 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
-
+import "./index.css";     // <-- must exist
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
